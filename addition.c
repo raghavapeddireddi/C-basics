79b8c42f295c20af37fc787e21dc5694a13eb6ea
raghavapeddireddi <92288097+raghavapeddireddi@users.noreply.github.com>
2021-10-25 10:46:09 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{ 
+int phy,che,mat,eng,tot;
+phy=45;
+che=88;
+mat=66;
+eng=99;
+tot=phy+che+mat+eng;
+printf("marks in physics: %d\n", phy);
+printf("marks in chemistry: %d\n", che);
+printf("marks in maths: %d\n", mat);
+printf("marks in english: %d\n", eng);
+printf("total marks %d\n", tot);
+}
+
